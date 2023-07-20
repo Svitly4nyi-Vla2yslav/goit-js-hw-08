426057edefa59580abb3a77f70358008defc135b
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-07-20 22:40:47 +0200

    03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,38 @@
 import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
 const FORM_KEY = 'feedback-form-state';
+handlerFillForm();
 function onFormInput(evt) {
   let formInfa = localStorage.getItem(FORM_KEY);
   formInfa = formInfa ? JSON.parse(formInfa) : {};
   formInfa[evt.target.name] = evt.target.value;
   localStorage.setItem(FORM_KEY, JSON.stringify(formInfa));
 }
-formEl.addEventListener('input', throttle(onFormInput, 500));
+
 function handlerFillForm() {
   let saveInfa = localStorage.getItem(FORM_KEY);
   if (saveInfa) {
     saveInfa = JSON.parse(saveInfa);
+
+    Object.entries(saveInfa).forEach(([key, text]) => {
+      formEl.elements[key].value = text || '';
+    });
   }
-  Object.entries(saveInfa).forEach(([key, text]) => {
-    formEl.elements[key].value = text;
-  });
 }
-handlerFillForm();
 function onFormSubmit(evt) {
   evt.preventDefault();
-  const {elements: {email, message}} = evt.currentTarget;
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  if (email.value === '' || message.value === '') {
+    return alert('fill all the fields');
+  } else {
+    let formData = JSON.parse(localStorage.getItem(FORM_KEY));
+    console.log(formData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(FORM_KEY);
+    formData = {};
+  }
 }
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
