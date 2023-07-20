fe4181ec23f0e1fb6200e86b7a1919e81af9b1ab
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-07-21 00:02:50 +0200

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import { async } from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframePlayer = document.querySelector('#vimeo-player');
+const player = new Player(iframePlayer);
+
+const saveCurrentTime = time => {
+  localStorage.setItem('videoplayer-current-time', time);
+};
+const restoreCurrentTime = async () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    await player.setCurrentTime(savedTime);
+  }
+};
+player.on(
+  'timeupdate',
+  throttle(evt => {
+    const currentTime = evt.seconds;
+    saveCurrentTime(currentTime);
+  }, 1000)
+);
+player.ready().then(restoreCurrentTime);
 
