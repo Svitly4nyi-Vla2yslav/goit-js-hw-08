8562c7e6c852dd6a9c130de2f121b2ad95611f62
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-07-21 19:17:10 +0200

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,63 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-// Описаний в документації
+
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
+
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const gallery = document.querySelector('.gallery');
-let currentModal = null;
-
 function renderGallery() {
+  const gallery = document.querySelector('.gallery');
   const galleryList = galleryItems
     .map(
       item => `
-    <li class='gallery__item'>
-        <a class='gallery__link' href='${item.original}'>
-            <img class='gallery__image'
-                src='${item.preview}'
-                data-source='${item.original}'
-                alt='${item.description}'
-            />
-        </a>
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>
     </li>
-`
+  `
     )
     .join('');
   gallery.insertAdjacentHTML('beforeend', galleryList);
 }
-
-function openModal(imgSource) {
-  const modal = SimpleLightbox.create(`
-        <img src='${imgSource}' />
-   `);
-    modal.show();
-    currentModal = modal;
-    document.addEventListener('keydown', onEsc)
-}
-
-
-function handleGalleryClick(evt) {
-  evt.preventDefault();
-  const target = evt.target;
-  if (target.classList.contains('gallery__image')) {
-    const imgSource = target.dataset.source;
-      openModal(imgSource);
-  }
-}
-function onEsc(evt) {
-  if (evt.code === 'Escape' && currentModal !== null) {
-      currentModal.close();
-      currentModal = null;
-      document.removeEventListener('keydown', onEsc);
-  }
-    
+document.addEventListener('DOMContentLoaded', function () {
   
-}
-document
-  .querySelector('.gallery')
-  .addEventListener('click', handleGalleryClick);
-window.addEventListener('DOMContentLoaded', renderGallery);
+  renderGallery();
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+  });
+});
 console.log(galleryItems);
 
